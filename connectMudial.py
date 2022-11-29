c2a044b454f66784f7d6bf349a7955592fada060
[emirand20] <[emirand20@jaumebalmes.net]>
2022-11-29 19:55:54 +0100

    connectHecho

--- connectMudial.py
+++ connectMudial.py
@@ -1,16 +1,16 @@
 import psycopg2
 
-conexion = psycopg2.connect(user='nuevo',
-                             password='1234567',
-                             host='localhost',
-                             port='5432',
-                             database='db_mundial')
-                             
+conexion = psycopg2.connect(user='postgresUser',
+                            password='postgresPW',
+                            host='172.16.46.189',
+                            port='5455',
+                            database='postgresDB')
+
 # creamos el metodo cursor
 cursor = conexion.cursor()
 
 # creamos sentencia sql
-sql = 'SELECT * FROM grupoA'
+sql = 'SELECT * FROM public."grupoA"'
 
 # ejecutamos metodo execute
 cursor.execute(sql)
